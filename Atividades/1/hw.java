7c6a84aca2927b5dca4c1d5e43e589ae2baa912c
Leonardo <reisleo619@gmail.com>
2026-01-14 15:40:27 -0300

    primeiro commit do projeto

---
+++ Atividades/1/hw.java
@@ -0,0 +1,13 @@
+// ------------------------------ CRIAÇÃO DA CLASSE --------------------------
+public class hw {
+// ------------------------------ CRIAÇÃO DA MAIN ----------------------------    
+    public static void main(String[] args) {
+// ------------------------------ CRIAÇAO DE VARIAVEL TIPO STRING ------------
+        String solicitante = args[0];
+        if (solicitante.equalsIgnoreCase("Professor")) {
+            System.out.println("Obrigado por me ensinar essa maravilha odiada por todos, espero me torna um grande Dev apartir de seus ensinamentos.");
+        }
+        else if (solicitante.equalsIgnoreCase("Estranho"));
+            System.out.printf(args[0] + " " + "SAI PRA LÁ CURIOSO");
+    }
+}
