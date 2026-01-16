5f36f07cd636e363bc6f02ba6a7ee4fe25e31855
Leonardo <reisleo619@gmail.com>
2026-01-16 15:38:24 -0300

    Treino

---
+++ tarefa_wrapper/src/Classes/Casting.java
@@ -0,0 +1,20 @@
+package tarefa_wrapper.src.Classes;
+
+/***
+ * @author Leonardo Pereira
+ */
+
+// Criando a classe
+public class Casting {
+
+// Criando a classe
+    public static void main(String[] args) {
+
+// Declarando as variáveis
+        int idade = 22;
+        Integer idade2 = 44;
+
+// Realizando o prind do console
+        System.out.print(idade + " " + (idade2));
+    }
+}
